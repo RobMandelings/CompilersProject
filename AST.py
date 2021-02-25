0c2719b11124b5226bf789cec8ec1ea66923ff70
Rob Mandelings <rob@minetronic.com>
2021-02-25 17:32:12 +0100

    Added todo and basic structure for AST made

---
+++ AST.py
@@ -0,0 +1,8 @@
+class AST:
+
+    def __init__(self):
+        self.children = list()
+        print(self.function())
+
+    def function(self):
+        return "Hello world"
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
 from antlr4 import *
+
+from AST import AST
 from CLexer import CLexer
 from CParser import CParser
- 
+
+# TODO order of operations
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = CLexer(input_stream)
@@ -11,6 +14,8 @@ def main(argv):
     tree = parser.prog()
 
     print(argv[1])
+
+    ast = AST()
  
 if __name__ == '__main__':
     main(sys.argv)
