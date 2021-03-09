6cd611e73eda5d588e49d90f3ea6e2accc0fae48
Jasper De Laet <jdlaet@gmail.com>
2021-03-09 16:49:04 +0100

    Syntactical analysis

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from CSTVisitors import CSTVisitorToDot
 from src.ast.CSTtoASTConverter import *
 from src.ast.ASTVisitors import ASTVisitorDot
+from src.syntacticalAnalysis import CSTErrorListener
 
 # TODO support for unary operations ('-5' for example)
 def main(argv):
@@ -9,19 +10,29 @@ def main(argv):
 
     stream = CommonTokenStream(lexer)
     parser = CParser(stream)
-    tree = parser.program()
 
-    cst_visitor_to_dot = CSTVisitorToDot()
-    tree.accept(cst_visitor_to_dot)
-    cst_visitor_to_dot.graph.render('output/cst.gv', view=False)
+    parser.removeErrorListeners()
+    parser.addErrorListener(CSTErrorListener())
+
+    try:
+        tree = parser.program()
+
+        cst_visitor_to_dot = CSTVisitorToDot()
+        tree.accept(cst_visitor_to_dot)
+        cst_visitor_to_dot.graph.render('output/cst.gv', view=False)
 
 
-    ast = createASTFromConcreteSyntaxTree(tree, lexer)
-    ast_visitor_dot = ASTVisitorDot()
-    ast.accept(ast_visitor_dot)
+        ast = createASTFromConcreteSyntaxTree(tree, lexer)
+        ast_visitor_dot = ASTVisitorDot()
+        ast.accept(ast_visitor_dot)
 
-    ast_visitor_dot.graph.render('output/ast.gv', view=False)
-    print(argv[1])
+        ast_visitor_dot.graph.render('output/ast.gv', view=False)
+        print(argv[1])
+
+    except SyntaxError:
+        print("Exiting program...", file=sys.stderr)
+        sys.exit()
+
 
 
 if __name__ == '__main__':
