afa50f14b6d0b649cd264719750f0b512dbd27fa
Jasper De Laet <jdlaet@gmail.com>
2021-03-12 16:08:43 +0100

    Single line comments

--- src/antlr4_gen/CParser.py
+++ src/antlr4_gen/CParser.py
@@ -11,7 +11,7 @@ else:
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\26")
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\27")
         buf.write("\u008a\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
         buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16")
         buf.write("\t\16\4\17\t\17\3\2\3\2\3\3\6\3\"\n\3\r\3\16\3#\3\4\3")
@@ -77,7 +77,8 @@ class CParser ( Parser ):
                       "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                       "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
                       "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                      "<INVALID>", "ID", "INTEGER", "DOUBLE", "WS" ]
+                      "<INVALID>", "ID", "INTEGER", "DOUBLE", "LineComment", 
+                      "WS" ]
 
     RULE_program = 0
     RULE_instructions = 1
@@ -119,7 +120,8 @@ class CParser ( Parser ):
     ID=17
     INTEGER=18
     DOUBLE=19
-    WS=20
+    LineComment=20
+    WS=21
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
