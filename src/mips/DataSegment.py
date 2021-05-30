2b27eb31aa6b3bcbbf8c4e643988337e99757bf1
Rob Mandelings <rob@minetronic.com>
2021-05-30 11:56:08 +0200

    DataSegment updated

--- src/mips/DataSegment.py
+++ src/mips/DataSegment.py
@@ -1,9 +1,21 @@
-
 class DataSegment:
 
     def __init__(self):
-        self.lines = list()
+        """
+        string_lines: lines in the data segment which have an ascii or asciiz 'data type'
+        floating_point_lines: lines in the data segment which have a .float as data type
+        """
+        self.ascii_data = list()
+        self.floating_point_data = list()
         self.printf_strings = dict()
 
     def add_printf_string(self, key, value: list):
         self.printf_strings[key] = value
+
+    def add_floating_point_number(self, value: float):
+        """
+        Adds a floating point number to the data, and returns the identifier of the added data
+        """
+        identifier = f"floating_point{len(self.floating_point_data)}"
+        self.floating_point_data.append(f'{identifier}: .float {value}')
+        return identifier
