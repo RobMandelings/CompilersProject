97814ed11041c3256cc9467b7743abd3813cd6b5
Rob Mandelings <rob@minetronic.com>
2021-05-30 22:55:39 +0200

    Prepared for uploading

--- src/main.py
+++ src/main.py
@@ -8,16 +8,19 @@ from src.syntacticalAnalysis import CSTErrorListener
 from src.mips.LLVMToMipsVisitor import LLVMToMipsVisitor
 
 
-# TODO support for unary operations ('-5' for example)
-
 def log(file, log_string):
     f = open(file, "a")
     f.write(log_string)
     f.close()
 
 
-def benchmark_file(file, filename):
-    input_stream = FileStream(file)
+def run_benchmark(filename: str):
+    """
+    filename: the filename you need to enter to run the script
+    to_llvm: true if compiling to llvm, false if compiling to mips
+    """
+
+    input_stream = FileStream(filename)
     lexer = CLexer(input_stream)
 
     stream = CommonTokenStream(lexer)
@@ -31,98 +34,47 @@ def benchmark_file(file, filename):
 
         cst_visitor_to_dot = CSTVisitorToDot()
         tree.accept(cst_visitor_to_dot)
-        cst_visitor_to_dot.graph.render('output/cst.gv', view=False)
+        cst_visitor_to_dot.graph.render(f'{filename}_cst.gv', view=False)
 
         try:
             ast = create_ast_from_cst(tree)
             ast_visitor_dot = ASTVisitorDot()
             ast.accept(ast_visitor_dot)
-            ast_visitor_dot.graph.render('output/ast.gv', view=False)
+            ast_visitor_dot.graph.render(f'{filename}_ast.gv', view=False)
 
             ast_visitor_semantic_analysis = ASTVisitorSemanticAnalysis(optimize=False)
             ast.accept(ast_visitor_semantic_analysis)
 
             ast_visitor_dot.reset()
             ast.accept(ast_visitor_dot)
-            ast_visitor_dot.graph.render('output/ast-optimized.gv', view=False)
+            ast_visitor_dot.graph.render(f'{filename}_ast-optimized.gv', view=False)
 
             ast_visitor_to_llvm = ASTVisitorToLLVM()
             ast.accept(ast_visitor_to_llvm)
-            ast_visitor_to_llvm.to_file("testinput/output.ll")
+
+            ast_visitor_to_llvm.to_file(f"{filename}_output.ll")
+
+            llvm_code = ast_visitor_to_llvm.get_builder().build()
+            llvm_to_mips_visitor = LLVMToMipsVisitor()
+            llvm_code.accept(llvm_to_mips_visitor)
+
+            llvm_to_mips_visitor.get_mips_builder().to_file('testinput/mips.asm')
+
 
         except SemanticError as e:
-            semanticErrorFile = "input/SemanticErrorLog"
-            log(semanticErrorFile, filename + ": ")
-            log(semanticErrorFile, "A semantic error occurred: ")
-            log(semanticErrorFile, e.args[0])
-            log(semanticErrorFile, "\n")
+            print("A semantic error occurred: ")
+            print(e)
+            print("Stopping the compiler...")
+            sys.exit(0)
 
     except SyntaxError:
-        syntaxErrorFile = "input/SyntaxErrorLog"
-        log(syntaxErrorFile, filename + ": ")
-        log(syntaxErrorFile, "Syntax error occured:")
-        log(syntaxErrorFile, sys.stderr)
+        print("Exiting program...", file=sys.stderr)
+        sys.exit()
 
 
 def main(argv):
-    benchmark = False
-
-    if benchmark:
-        directory = "input/SemanticErrors"
-        for filename in os.listdir(directory):
-            filepath = os.path.join(directory, filename)
-            print(filename)
-            benchmark_file(filepath, filename)
-    else:
-        input_stream = FileStream(argv[1])
-        lexer = CLexer(input_stream)
-
-        stream = CommonTokenStream(lexer)
-        parser = CParser(stream)
-
-        parser.removeErrorListeners()
-        parser.addErrorListener(CSTErrorListener())
-
-        try:
-            tree = parser.program()
-
-            cst_visitor_to_dot = CSTVisitorToDot()
-            tree.accept(cst_visitor_to_dot)
-            cst_visitor_to_dot.graph.render('output/cst.gv', view=False)
-
-            try:
-                ast = create_ast_from_cst(tree)
-                ast_visitor_dot = ASTVisitorDot()
-                ast.accept(ast_visitor_dot)
-                ast_visitor_dot.graph.render('output/ast.gv', view=False)
-
-                ast_visitor_semantic_analysis = ASTVisitorSemanticAnalysis(optimize=False)
-                ast.accept(ast_visitor_semantic_analysis)
-
-                ast_visitor_dot.reset()
-                ast.accept(ast_visitor_dot)
-                ast_visitor_dot.graph.render('output/ast-optimized.gv', view=False)
-
-                ast_visitor_to_llvm = ASTVisitorToLLVM()
-                ast.accept(ast_visitor_to_llvm)
-                ast_visitor_to_llvm.to_file("testinput/output.ll")
-
-                llvm_code = ast_visitor_to_llvm.get_builder().build()
-                llvm_to_mips_visitor = LLVMToMipsVisitor()
-                llvm_code.accept(llvm_to_mips_visitor)
-
-                llvm_to_mips_visitor.get_mips_builder().to_file('testinput/mips.asm')
-
-
-            except SemanticError as e:
-                print("A semantic error occurred: ")
-                print(e)
-                print("Stopping the compiler...")
-                sys.exit(0)
-
-        except SyntaxError:
-            print("Exiting program...", file=sys.stderr)
-            sys.exit()
+    filename = argv[1]
+    run_benchmark(filename)
 
 
 if __name__ == '__main__':
